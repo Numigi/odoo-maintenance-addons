b88f322e540b77c4fdcc9e54563dd19f4ef37615
Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>
2022-06-20 14:44:56 -0400

    add to docker_files

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,7 +11,7 @@
     'category': 'Other',
     'summary': 'Install all addons required for testing.',
     'depends': [
-
+        'maintenance_equipment_model',
     ],
     'installable': True,
 }
